6aca84ff07678d480561af8b19d212c195a03488
KiwiUserSFTW <danielkostenko0305@gmail.com>
2025-08-30 19:03:05 +0200

    exercises1.1 has been completed

---
+++ part1/README.md
@@ -0,0 +1,8 @@
+# Part 1 - Introduction to React
+
+## Exercises 1.1 - 1.14
+
+### Practical
+- [] new note diagram
+- [] spa diagram
+- [] spa new note diagram
---
+++ part1/exercises1.1-/src/main.jsx
@@ -0,0 +1,7 @@
+// react
+import ReactDOM from "react-dom/client";
+
+// components
+import App from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
