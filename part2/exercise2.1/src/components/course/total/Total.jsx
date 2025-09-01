be0b148a5cf961880f366a97f26ce7c6edcf5deb
KiwiUserSFTW <danielkostenko0305@gmail.com>
2025-09-01 19:10:21 +0200

    exercise2.3* has been completed

--- part2/exercise2.1/src/components/course/total/Total.jsx
+++ part2/exercise2.1/src/components/course/total/Total.jsx
@@ -1,9 +1,6 @@
 const Total = (props) => {
   // reduce array method to calculate summary
-  const summary = props.parts.reduce((sum, part) => {
-    sum += part.exercises;
-    return sum;
-  }, 0);
+  const summary = props.parts.reduce((sum, part) => (sum += part.exercises), 0);
 
   return <h4>total of {summary} exercises</h4>;
 };
