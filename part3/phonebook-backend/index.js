d36e028a53fbec6b7b7ecd5144cdb164709595c9
KiwiUserSFTW <danielkostenko0305@gmail.com>
2025-09-10 14:44:10 +0200

    exercise 3.13 has been completed

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -4,6 +4,9 @@ const express = require("express");
 // middlewares
 const morgan = require("morgan");
 
+// mongodb
+const Person = require("./models/person");
+
 morgan.token("postbody", (request, response) => {
   if (request.method === "POST") {
     return JSON.stringify(request.body);
@@ -22,74 +25,45 @@ app.use(
   )
 );
 
-// functions
-// randomizer for new id
-const getRandomId = (existingIds, maxId = 1000) => {
-  const id = String(Math.floor(Math.random() * maxId));
-
-  // check id for uniquelity
-  if (existingIds.includes(id)) return getRandomId(existingIds, maxId);
-  return id;
-};
-
-// data
-let persons = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: "3",
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
 app.get("/", (request, response) => {
   response.send("<h1> Persons page </h1>");
 });
 
 // get info page
 app.get("/info", (request, response) => {
-  const personsLength = persons.length;
-  const date = new Date();
-  const body = `
-  <div>
-  <h3> Phonebook has info for ${personsLength} people</h3>
-  <h3> ${date} </h3>
-  </div>
-  `;
-  response.send(body);
+  Person.find({}).then((persons) => {
+    const personsLength = persons.length;
+    const date = new Date();
+    const body = `
+    <div>
+    <h3> Phonebook has info for ${personsLength} people</h3>
+    <h3> ${date} </h3>
+    </div>
+    `;
+    response.send(body);
+  });
 });
 
 // get all persons
 app.get("/api/persons", (request, response) => {
-  response.json(persons);
+  Person.find({}).then((persons) => {
+    response.json(persons);
+  });
 });
 
 // get one person
 app.get("/api/persons/:id", (request, response) => {
   const id = request.params.id;
-  const person = persons.find((p) => p.id === id);
 
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).json({
-      error: "person doesn't exist",
+  Person.findById(id)
+    .then((person) => {
+      response.json(person);
+    })
+    .catch((error) => {
+      response.status(404).json({
+        error: "person doesn't exist",
+      });
     });
-  }
 });
 
 // delete one person
---
+++ part3/phonebook-backend/models/person.js
@@ -0,0 +1,33 @@
+// db
+const mongoose = require("mongoose");
+
+// additional
+require("dotenv").config();
+
+const url = process.env.MONGODB_URL;
+
+mongoose.set("strictQuery", false);
+mongoose
+  .connect(url)
+  .then(() => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("error connecting to MongoDB:", error.message);
+  });
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+personSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+    console.log(document);
+  },
+});
+
+module.exports = mongoose.model("Person", personSchema);
