625e01277461233769144abc3d45cf42ce79747d
KiwiUserSFTW <danielkostenko0305@gmail.com>
2025-10-12 20:58:42 +0200

    exercise4.10 has been completed

--- part4/blog-list/tests/api.test.js
+++ part4/blog-list/tests/api.test.js
@@ -25,11 +25,11 @@ beforeEach(async () => {
 
 describe("api", () => {
   test("get return correct data", async () => {
-    const blogsAtEnd = await api
+    const blogsResponse = await api
       .get("/api/blogs")
       .expect(200)
       .expect("Content-Type", /application\/json/);
-    assert.strictEqual(blogsAtEnd.body.length, blogs.length);
+    assert.strictEqual(blogsResponse.body.length, blogs.length);
   });
   test("blog obj have id instead of _id", async () => {
     const blogsResponse = await api.get("/api/blogs").expect(200);
@@ -39,6 +39,23 @@ describe("api", () => {
       assert.ok("id" in blog);
       assert.ok(!("_id" in blog));
     });
+  });
+  test("post add new blog", async () => {
+    const newBlog = {
+      title: "testBlog",
+      author: "Edsger W. Dijkstra",
+      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+      likes: 1,
+    };
+
+    await api.post("/api/blogs").send(newBlog).expect(201);
+
+    const blogsResponse = await api.get("/api/blogs").expect(200);
+    const blogsBody = blogsResponse.body;
+    const createdBlog = blogsBody.find((b) => b.title === newBlog.title);
+
+    assert.strictEqual(blogsBody.length, blogs.length + 1);
+    assert.ok(createdBlog);
   });
 });
 
