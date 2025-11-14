c4f36d492ef449e443a873cee20e55fe626254bf
KiwiUserSFTW <danielkostenko0305@gmail.com>
2025-11-14 09:33:03 +0100

    git sync

--- part5/playwright/tests/blogs_app.spec.js
+++ part5/playwright/tests/blogs_app.spec.js
@@ -10,9 +10,6 @@ const test = base.extend({
 });
 
 test.describe("Blog list", () => {
-  test.beforeEach(async ({ page }) => {
-    await page.goto(url);
-  });
   test.describe("user not logged in", () => {
     test("has login form by default", async ({ page }) => {
       const username = page.getByLabel("username");
@@ -27,21 +24,37 @@ test.describe("Blog list", () => {
   test.describe("one user exist in db", () => {
     let user = { username: "", password: "", name: "" };
     test.beforeEach(async ({ request }) => {
+      // creating one fresh user to clean dbf
       await resetDb(request);
       user = await createUser(request);
     });
-    test("login with valid credentials", async ({ page }) => {
-      // login using form
-      await login(page, user);
 
-      await expect(page.getByText(`${user.name} logged in`)).toBeVisible();
+    test.describe("When logged in", () => {
+      test.beforeEach(async ({ page }) => {
+        // login using form
+        await login(page, user);
+      });
+
+
     });
-    test("login fails with wrong credentials", async ({ page }) => {
-      // login using form with wrong creds
-      await login(page, { ...user, password: "wrong password" });
 
-      await expect(page.locator(".error")).toContainText("wrong credentials");
-      await expect(page.getByText(`${user.name} logged in`)).not.toBeVisible();
+
+    test.describe("Login", () => {
+      test("'succeeds with correct credentials'", async ({ page }) => {
+        // login using form
+        await login(page, user);
+
+        await expect(page.getByText(`${user.name} logged in`)).toBeVisible();
+      });
+      test("fails with wrong credentials", async ({ page }) => {
+        // login using form with wrong creds
+        await login(page, { ...user, password: "wrong password" });
+
+        await expect(page.locator(".error")).toContainText("wrong credentials");
+        await expect(
+          page.getByText(`${user.name} logged in`)
+        ).not.toBeVisible();
+      });
     });
   });
 });
