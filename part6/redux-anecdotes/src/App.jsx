9bee4d5b440fdfe2b18a1b6202cc6484965e0226
KiwiUserSFTW <danielkostenko0305@gmail.com>
2025-12-26 20:33:00 +0100

    exercise 6.14 has been completed

--- part6/redux-anecdotes/src/App.jsx
+++ part6/redux-anecdotes/src/App.jsx
@@ -1,3 +1,15 @@
+// react
+import { useEffect } from "react";
+
+// redux
+import { useDispatch } from "react-redux";
+
+// reducer
+import { setAnecdotes } from "./reducers/anecdoteReducer/anecdoteReducer.js";
+
+// services
+import anecdoteService from "./services/anecdote.js";
+
 // components
 import AnecdoteList from "./components/Anecdotes/AnecdoteList/AnecdoteList";
 import AnecdoteForm from "./components/Anecdotes/AnecdoteForm";
@@ -7,6 +19,13 @@ import Filter from "./components/Anecdotes/Filter/Filter";
 // import Anecdotes from "./components/Anecdotes/Anecdotes";
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    anecdoteService
+      .getAll()
+      .then((anecdotes) => dispatch(setAnecdotes(anecdotes)));
+  });
   return (
     <div>
       <h2>Anecdotes</h2>
--- part6/redux-anecdotes/src/reducers/anecdoteReducer/anecdoteReducer.test.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer/anecdoteReducer.test.js
@@ -1,6 +1,15 @@
 // reducers
 import anecdoteReducer, { addAnecdote, vote } from "./anecdoteReducer";
 
+// const initAnecdotes = [
+//   "If it hurts, do it more often",
+//   "Adding manpower to a late software project makes it later!",
+//   "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+//   "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+//   "Premature optimization is the root of all evil.",
+//   "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+// ];
+
 describe("anecdote reducer", () => {
   test("anecdote can be voted with action anecdote/vote", () => {
     const state = anecdoteReducer(undefined, "NO_ACTION");
