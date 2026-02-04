41d7e378f5876514c902693df1c01998f964bda0
KiwiUserSFTW <danielkostenko0305@gmail.com>
2026-02-04 16:39:35 +0100

    react-queary configured for exercise 7.11

--- part7/bloglist-query/src/main.jsx
+++ part7/bloglist-query/src/main.jsx
@@ -1,14 +1,21 @@
 // react
 import ReactDOM from "react-dom/client";
 
+// query
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 // componens
 import App from "./App";
 
 // context
 import { NotificationContextProvider } from "./context/notificationContext";
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <NotificationContextProvider>
-    <App />
-  </NotificationContextProvider>,
+  <QueryClientProvider client={queryClient}>
+    <NotificationContextProvider>
+      <App />
+    </NotificationContextProvider>
+  </QueryClientProvider>,
 );
