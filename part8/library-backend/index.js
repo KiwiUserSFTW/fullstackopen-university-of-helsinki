5b22afd30b1bfbcce0c75590dd6281daabc8812a
KiwiUserSFTW <danielkostenko0305@gmail.com>
2026-03-25 14:43:51 +0100

    exercise 8.6 has been completed

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require("@apollo/server");
 const { startStandaloneServer } = require("@apollo/server/standalone");
+const { v1: uuid } = require("uuid");
 
 let authors = [
   {
@@ -99,6 +100,7 @@ const typeDefs = /* GraphQL */ `
     author: String!
     published: Int!
     genres: [String!]!
+    id: ID!
   }
 
   type Author {
@@ -113,6 +115,15 @@ const typeDefs = /* GraphQL */ `
     authorCount: Int!
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
+  }
+
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      published: Int!
+      genres: [String!]!
+    ): Book
   }
 `;
 
@@ -153,6 +164,22 @@ const resolvers = {
 
         return { ...author, bookCount };
       }),
+  },
+  Mutation: {
+    addBook: (root, args) => {
+      const newBook = { ...args, id: uuid() };
+
+      const authorExist = authors.some(
+        (author) => author.name === newBook.author
+      );
+
+      if (!authorExist) {
+        authors = authors.concat({ name: newBook.author, id: uuid() });
+      }
+
+      books = books.concat(newBook);
+      return newBook;
+    },
   },
 };
 
