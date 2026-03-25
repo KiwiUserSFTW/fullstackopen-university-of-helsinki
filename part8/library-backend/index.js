decf7b0e4be1115b1f29d616068d1ab4ddcfe3d6
KiwiUserSFTW <danielkostenko0305@gmail.com>
2026-03-25 12:58:24 +0100

    exercise 8.4 has been completed

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -111,7 +111,7 @@ const typeDefs = /* GraphQL */ `
   type Query {
     bookCount: Int!
     authorCount: Int!
-    allBooks: [Book!]!
+    allBooks(author: String): [Book!]!
     allAuthors: [Author!]!
   }
 `;
@@ -120,7 +120,8 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books,
+    allBooks: (root, { author }) =>
+      author ? books.filter((book) => book.author === author) : books,
     allAuthors: () =>
       authors.map((author) => {
         const bookCount = books.filter(
